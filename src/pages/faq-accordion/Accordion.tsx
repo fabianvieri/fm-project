0a79533e3713053b5086574b353aa9b03a36239d
fabianvieri <vierifabian@gmail.com>
2024-03-25 22:08:39 +0700

    try deploy svg astro

---
+++ src/pages/faq-accordion/Accordion.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'preact/hooks';
+
+import Plus from './images/icon-plus.svg';
+import Minus from './images/icon-minus.svg';
+import classes from './accordion.module.css';
+
+interface Props {
+	data: { label: string; content: string }[];
+}
+
+const Accordion = ({ data }: Props) => {
+	const [open, setOpen] = useState<boolean[]>(Array(data.length).fill(false));
+
+	const openHandler = (index: number) => {
+		setOpen((prevState) => {
+			const newState = [...prevState];
+			newState[index] = !newState[index];
+			return newState;
+		});
+	};
+
+	return (
+		<div>
+			{data.map(({ label, content }, i) => (
+				<div
+					key={i}
+					className={`${classes.accordion} ${
+						open[i] ? classes.active : ''
+					} p-4`}
+				>
+					<div className="flex justify-between items-center gap-4">
+						<h2 className="font-bold" onClick={() => openHandler(i)}>
+							{label}
+						</h2>
+						{open[i] ? (
+							<img src={Minus.src} alt="minus" className="shrink-0" />
+						) : (
+							<img src={Plus.src} alt="plus" className="shrink-0" />
+						)}
+					</div>
+					<p className={`${classes.textGrayPurple} ${classes.content} mt-4`}>
+						{content}
+					</p>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Accordion;
